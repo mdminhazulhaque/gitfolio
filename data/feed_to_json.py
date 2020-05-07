25fa2b103b66717cd925e08072861b9a86e01579
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2020-05-07 21:07:43 +0800

    Script to generate blog.json from feed.xml

---
+++ data/feed_to_json.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import dateutil.parser
+
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+
+content = requests.get("https://bits.mdminhazulhaque.io/feed.xml").text
+soup = BeautifulSoup(content, "lxml")
+entries = soup.find_all("entry")
+
+data = []
+
+for entry in entries[:4]:
+    link = entry.link['href'].replace("http://localhost:4000/", "https://bits.mdminhazulhaque.io/")
+    
+    blog = {
+        "url_title": link,
+        "title": entry.title.text,
+        "sub_title": entry.summary.text,
+        "date": dateutil.parser.isoparse(entry.published.text).strftime("%d %b %Y"),
+        "tag": link.split("/")[3],
+        "visible": True
+    }
+    
+    data.append(blog)
+
+with open("blog.json", "w") as fp:
+    json.dump(data, fp, indent=4)
