beef839dc91086081f9b4c8909b2fbaed95d9c47
Md Minhazul Haque <mdminhazulhaque@gmail.com>
2020-08-02 22:03:13 +0800

    New XML Fetch

--- fetch_blog.py
+++ fetch_blog.py
@@ -1,30 +1,21 @@
+import xmltodict
 import json
 import requests
-import dateutil.parser
 
-try: 
-    from BeautifulSoup import BeautifulSoup
-except ImportError:
-    from bs4 import BeautifulSoup
-
-content = requests.get("https://bits.mdminhazulhaque.io/feed.xml").text
-soup = BeautifulSoup(content, "lxml")
-entries = soup.find_all("entry")
+xmldata = requests.get("https://bits.mdminhazulhaque.io/feed.xml").text
+jsondata = xmltodict.parse(xmldata)
 
 data = []
 
-for entry in entries[:4]:
-    link = entry.link['href']
-    
+for entry in jsondata['feed']['entry'][:4]:
     blog = {
-        "url_title": link,
-        "title": entry.title.text,
-        "sub_title": entry.summary.text,
-        "date": dateutil.parser.isoparse(entry.published.text).strftime("%d %b %Y"),
-        "tag": link.split("/")[3],
+        "url_title": entry['link']['@href'],
+        "title": entry['title']['#text'],
+        "sub_title": entry['summary']['#text'],
+        "date": entry['published'][:10],
+        "tag": entry['link']['@href'].split("/")[3],
         "visible": True
     }
-    
     data.append(blog)
 
 with open("data/blog.json", "w") as fp:
